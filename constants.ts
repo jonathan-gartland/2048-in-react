b52da494d05fd58e280bf402f9dff5074fe9c0a5
jonathan-gartland <jonathan-gartland@gmail.com>
2024-08-09 15:11:04 -0400

    playing with fork

--- constants.ts
+++ constants.ts
@@ -13,3 +13,6 @@ export const tileCountPerDimension = 4;
 export const mergeAnimationDuration = 100; // ms
 
 export const moveAnimationDuration = 200; // ms
+
+export const dummyAnimationDuration= 600; // ms
+
